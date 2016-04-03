e1a113ed0db1a87253e75ca6bb2e8811f52eb7f6
Kiran Gadhave <kiran@kirangadhave.in>
2016-04-03 10:51:05 +0530

    Added Todos

--- server.js
+++ server.js
@@ -2,15 +2,35 @@ var express = require('express'),
 	app		= express(),
 	PORT	= process.env.PORT || 3000;
 
+var todos = [{
+	id: 1, 
+	desc: "Goto Office",
+	status: false
+}, {
+	id: 2,
+	desc: "Goto Market",
+	status: false
+}, {
+	id:3,
+	desc: "Goto HEll",
+	status: true
+}];
+
 app.get('/', function(req, res){
 	res.send('TODO API ROOT');
 });
 
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
 
+app.get('/todos/:id', function(req, res){
+	
+});
 
 app.listen(PORT, function(error){
 	if(error)
 		console.log('Error: ' + error);
 	else
 		console.log('Server up on ' + PORT);
-})
+});
